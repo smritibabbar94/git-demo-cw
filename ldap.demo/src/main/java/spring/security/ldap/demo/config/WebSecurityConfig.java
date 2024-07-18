60300a63f6c4861ea3bb6da46ccdeabd0e39a344
smritibabbar94 <98202602+smritibabbar94@users.noreply.github.com>
2024-07-18 09:58:22 +0530

    Update WebSecurityConfig.java

--- ldap.demo/src/main/java/spring/security/ldap/demo/config/WebSecurityConfig.java
+++ ldap.demo/src/main/java/spring/security/ldap/demo/config/WebSecurityConfig.java
@@ -1,59 +1,108 @@
+//package spring.security.ldap.demo.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.env.Environment;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import spring.security.ldap.demo.util.LdapAuthenticationProvider;
+//
+//
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurityConfig {
+//
+//    private Environment env;
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    public WebSecurityConfig(Environment env){
+//        this.env = env;
+//    }
+//
+//    /* configures the HttpSecurity object to require all requests to be authenticated and to use form login. */
+////    @Bean
+////    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+////        http
+////                .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable())
+////                .authorizeHttpRequests(authorize -> authorize
+////                        .requestMatchers("/authenticate").permitAll()
+////                        .anyRequest().authenticated()
+////                )
+////                .sessionManagement(session -> session
+////                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+////                )
+////                //.formLogin(Customizer.withDefaults());
+////                .httpBasic(Customizer.withDefaults());
+////
+////        return http.build();
+////    }
+//
+//    @Autowired
+//    public void configure(AuthenticationManagerBuilder auth) throws Exception
+//    {
+//        auth.authenticationProvider(new LdapAuthenticationProvider(env)).eraseCredentials(false);
+//    }
+//
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+//        return authenticationConfiguration.getAuthenticationManager();
+//    }
+//}
+
 package spring.security.ldap.demo.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.context.annotation.Bean;
 import org.springframework.beans.factory.annotation.Autowired;
-import spring.security.ldap.demo.util.LdapAuthenticationProvider;
 
 
 @Configuration
-@EnableWebSecurity
 public class WebSecurityConfig {
 
-    private Environment env;
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    public WebSecurityConfig(Environment env){
-        this.env = env;
-    }
-
-    /* configures the HttpSecurity object to require all requests to be authenticated and to use form login. */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable())
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/authenticate").permitAll()
-                        .anyRequest().authenticated()
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().fullyAuthenticated()
                 )
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
-                .httpBasic(Customizer.withDefaults());
+                .formLogin(Customizer.withDefaults());
 
         return http.build();
     }
 
     @Autowired
-    public void configure(AuthenticationManagerBuilder auth) throws Exception
-    {
-        auth.authenticationProvider(new LdapAuthenticationProvider(env)).eraseCredentials(false);
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .ldapAuthentication()
+                .userDnPatterns("uid={0},ou=people")
+                .groupSearchBase("ou=groups")
+                .contextSource()
+                .url("ldap://localhost:8389/dc=springframework,dc=org")
+                .and()
+                .passwordCompare()
+                .passwordEncoder(new BCryptPasswordEncoder())
+                .passwordAttribute("userPassword");
     }
 
     @Bean
